fc972b8978dd9ffbb118dfa5ecd6e949f7a48a8c
Alecaddd <castellani.ale@gmail.com>
2018-05-09 22:04:55 -0700

    Created meson building system

---
+++ meson/post_install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+install_prefix = os.environ['MESON_INSTALL_PREFIX']
+schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
+
+if not os.environ.get('DESTDIR'):
+	print('Compiling gsettings schemas...')
+	subprocess.call(['glib-compile-schemas', schemadir])
+	print('Renaming icons...')
+	for size in ['16x16', '24x24', '32x32', '64x64', '128x128']:
+		src = os.path.join(install_prefix, 'share', 'icons', 'hicolor', size, 'mimetypes', 'com.github.alecaddd.sequeler.svg')
+		dst = os.path.join(install_prefix, 'share', 'icons', 'hicolor', size, 'mimetypes', 'application-x-sequeler.svg')
+		os.rename(src, dst)
