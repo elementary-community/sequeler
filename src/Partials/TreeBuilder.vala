elementary-community/sequeler

commit c18244fe6c01a2a31b0f6a0b57bd060a2c8e21c4

do not sort on click

src/Partials/TreeBuilder.vala
@@ -57,7 +57,7 @@ public class Sequeler.Partials.TreeBuilder : Gtk.TreeView {
 			column.clickable = true;
 			column.resizable = true;
 			column.expand = true;
-			column.sort_column_id = col;
+
 			if (col > 0) {
 				column.sizing = Gtk.TreeViewColumnSizing.FIXED;
 				column.fixed_width = 150;
@@ -119,7 +119,9 @@ public class Sequeler.Partials.TreeBuilder : Gtk.TreeView {
 		store.set_value (iter, tot_columns, background);
 	}
 
-	public void redraw () {
+	public void redraw (Gtk.TreeViewColumn column) {
+		debug (column.title);
+		column.sort_indicator = true;
 		Gtk.TreeIter iter;
 		var i = 0;
 

commit 73b6db7d36c95a46671efb8e7268cfb14579a623

Fix column copy data
This was broken when sorting changed in commit c18244f

Fixed by setting a custom data property on the column.

src/Partials/TreeBuilder.vala
@@ -67,6 +67,7 @@ public class Sequeler.Partials.TreeBuilder : Gtk.TreeView {
             column.clickable = true;
             column.resizable = true;
             column.expand = true;
+            column.set_data ("model_column_id", col);
 
             if (col > 0) {
                 column.sizing = Gtk.TreeViewColumnSizing.FIXED;
@@ -155,7 +156,7 @@ public class Sequeler.Partials.TreeBuilder : Gtk.TreeView {
         Gdk.Display display = Gdk.Display.get_default ();
         Gtk.Clipboard clipboard = Gtk.Clipboard.get_default (display);
         model.get_iter (out iter, path);
-        model.get_value (iter, column.get_sort_column_id (), out val);
+        model.get_value (iter, column.get_data ("model_column_id"), out val);
 
         Gda.DataHandler handler = Gda.DataHandler.get_default (val.type ());
         string? column_data = handler.get_str_from_value (val);